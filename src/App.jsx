db38f3730f4d51a536c72db7ba8540e6932adbf6
harshmaitray-tech <harshmaitray@gmail.com>
2026-06-04 12:49:53 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -131,7 +131,7 @@ function App() {
       </ul>
 
       <footer className="footer">
-        Created by <span className="author">Harsh Maitray</span> ✨
+        Created by <span className="author">Harsh Maitray Ji</span> ✨
       </footer>
     </div>
   );
